add4286d13593b0743515becb07b7b0a2e39b417
Raphael Dahomay <raphael.dahomay@gmail.com>
2026-02-19 22:20:02 +0100

    continued developing forward_futures function

--- engine/deriv/fnf.py
+++ engine/deriv/fnf.py
@@ -4,10 +4,20 @@ from fredapi import Fred
 fred = Fred(api_key="acb2d179d51fb6dde4b7a8e3a4839ce3")
 
 
-def forward_future_prices(so, t, inc=0, q=0, storage=0, u=0, conv_y=0):
-    round_t = round(float(t), 0)
-    if float(t) != round_t:
-        low_t = round(float(t))
-        high_t = round(float(t) + 1)  # continue to code the logic around getting the low and t=high integer of any number
-    rf = fred.get_series("DGS1").values[-1]
-    return (so - inc + storage) * math.exp((rf - q + u - conv_y) * t)
+def forward_future_prices(so, t, inc=0, q=0, storage=0, u=0, conv_y=0):  # think of removing the NaN inside the DF rtrived
+    base_t = float(t)
+    if base_t < 1:
+        if base_t >= 0.5:
+            low_t = 0.5
+        sth = 0  # develop the logic
+    else:
+        int_t = int(base_t)
+        if base_t == int_t:
+            final_rf = fred.get_series(f"DGS{int_t}").values[-1]
+        else:
+            int_t_up = int_t + 1
+            rf_low = fred.get_series(f"DGS{int_t}").values[-1]
+            rf_up = fred.get_series(f"DGS{int_t_up}").values[-1]
+            spread = base_t - int_t
+            final_rf = rf_low + spread * (rf_up - rf_low)
+    return (so - inc + storage) * math.exp((final_rf - q + u - conv_y) * t)
